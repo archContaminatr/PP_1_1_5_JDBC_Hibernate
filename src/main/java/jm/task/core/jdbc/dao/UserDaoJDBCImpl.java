672f0e1c79d76d47ef55f16f70555c89e16e1b9a
archContaminatr <109415563+archContaminatr@users.noreply.github.com>
2023-03-29 04:34:30 +0600

    Update UserDaoJDBCImpl.java
    added transactions, IF NOT EXISTS, preparedStatement

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,32 +8,31 @@ import java.util.*;
 import static jm.task.core.jdbc.util.Util.getConnection;
 
 public class UserDaoJDBCImpl implements UserDao {
-
+    //Добавил транзакции только в INSERT/DELETE-методы
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-        String sqlCommand = "CREATE TABLE `pp-1.1.4`.`users` (\n" +
-                "  `id` BIGINT(45) NOT NULL AUTO_INCREMENT,\n" +
-                "  `name` VARCHAR(45) NOT NULL,\n" +
-                "  `lastname` VARCHAR(45) NOT NULL,\n" +
-                "  `age` TINYINT(45) NOT NULL,\n" +
-                "  PRIMARY KEY (`id`));";
-        try (Connection connection = getConnection()) {
-            Statement statement = connection.createStatement();
+        String sqlCommand = """
+                CREATE TABLE IF NOT EXISTS `pp-1.1.4`.`users` (
+                  `id` BIGINT(45) NOT NULL AUTO_INCREMENT,
+                  `name` VARCHAR(45) NOT NULL,
+                  `lastname` VARCHAR(45) NOT NULL,
+                  `age` TINYINT(45) NOT NULL,
+                  PRIMARY KEY (`id`));""";
+        try (Connection connection = getConnection(); Statement statement = connection.createStatement()) {
+            connection.setAutoCommit(false);
             statement.executeUpdate(sqlCommand);
             System.out.println("The table has been created");
-        } catch (SQLSyntaxErrorException ignore) { }
-        catch (SQLException e) {
-            e.printStackTrace();
+        } catch (SQLException e) {
+                e.printStackTrace();
         }
     }
 
     public void dropUsersTable() {
         String sqlCommand = "DROP TABLE users";
-        try (Connection connection = getConnection()) {
-            Statement statement = connection.createStatement();
+        try (Connection connection = getConnection(); Statement statement = connection.createStatement()) {
             statement.executeUpdate(sqlCommand);
             System.out.println("The table has been deleted");
         } catch (SQLSyntaxErrorException ignore) { }
@@ -43,12 +42,19 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String sqlCommand = "INSERT INTO users (name, lastName, age) VALUES ('"
-                + name + "', '" + lastName + "', '" + age + "')";
+        String sqlCommand = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
         try (Connection connection = getConnection()) {
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(sqlCommand);
-            System.out.println("User с именем – " + name + " добавлен в базу данных");
+            connection.setAutoCommit(false);
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sqlCommand)) {
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, lastName);
+                preparedStatement.setByte(3, age);
+                connection.commit();
+                System.out.println("User с именем – " + name + " добавлен в базу данных");
+            } catch (SQLException e) {
+                connection.rollback();
+                e.printStackTrace();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -57,9 +63,15 @@ public class UserDaoJDBCImpl implements UserDao {
     public void removeUserById(long id) {
         String sqlCommand = "DELETE FROM users WHERE id=" + id;
         try (Connection connection = getConnection()) {
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(sqlCommand);
-            System.out.println("User ID" + id + " has been deleted");
+            connection.setAutoCommit(false);
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(sqlCommand);
+                connection.commit();
+                System.out.println("User ID" + id + " has been deleted");
+            } catch (SQLException e) {
+                connection.rollback();
+                e.printStackTrace();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -67,8 +79,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public List<User> getAllUsers() {
         String sqlCommand = "SELECT * FROM users";
-        try (Connection connection = getConnection()) {
-            Statement statement = connection.createStatement();
+        try (Connection connection = getConnection(); Statement statement = connection.createStatement()) {
             ResultSet rs = statement.executeQuery(sqlCommand);
             List<User> resultList = new ArrayList<>();
 
@@ -78,7 +89,7 @@ public class UserDaoJDBCImpl implements UserDao {
                 Byte retrievedAge = rs.getByte("age");
                 User user = new User(retrievedName, retrievedLastName, retrievedAge);
                 resultList.add(user);
-                System.out.println(user.toString());
+                System.out.println(user);
             }
             return resultList;
         } catch (SQLException e) {
@@ -89,8 +100,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void cleanUsersTable() {
         String sqlCommand = "TRUNCATE TABLE users";
-        try (Connection connection = getConnection()) {
-            Statement statement = connection.createStatement();
+        try (Connection connection = getConnection(); Statement statement = connection.createStatement()) {
             statement.executeUpdate(sqlCommand);
             System.out.println("The table has been cleaned");
         } catch (SQLException e) {
